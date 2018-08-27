4757adb6f1e2100b9c2c3ac1196d8d0a14b4ae73
Martin M <mtendotnet@users.noreply.github.com>
2018-08-27 15:27:43 +0100

    feat: introduce styled-components and basic project styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Markdown from 'markdown-to-jsx'
 import AceEditor from 'react-ace';
+import styled from 'styled-components';
 import brace from 'brace';
 import 'brace/mode/markdown';
 import 'brace/theme/dracula';
@@ -28,20 +29,57 @@ class App extends Component {
 
     return (
       <div className="App">
-        <AceEditor
-          mode="markdown"
-          theme="dracula"
-          onChange={(newContent) => {
-            this.setState({ loadedFile: newContent });
-          }}
-          name="md-editor"
-          value={loadedFile}
-        />
-        
-        <Markdown>{loadedFile}</Markdown>
+        <Split>
+          <CodeWindow>
+            <AceEditor
+              mode="markdown"
+              theme="dracula"
+              onChange={(newContent) => {
+                this.setState({ loadedFile: newContent });
+              }}
+              name="md-editor"
+              value={loadedFile}
+            />
+          </CodeWindow>
+          <RenderedWindow>
+            <Markdown>{loadedFile}</Markdown>
+          </RenderedWindow>
+        </Split>
       </div>
     );
   }
 }
 
 export default App;
+
+const Split = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const CodeWindow = styled.div`
+  flex: 1;
+  padding-top: 2rem;
+  background-color: #191324;
+`;
+
+const RenderedWindow = styled.div`
+  background-color: #191324;
+  width: 35%;
+  padding: 20px;
+  color: #FFF;
+  border-left: 1px solid #302b3a
+  
+  h1, h2, h3, h4, h5, h6 {
+    color: #82d8d8;
+  }
+
+  h1 {
+    border-bottom: 3px solid #E54B4B;
+    padding-bottom: 10px;
+  }
+
+  a {
+    color: #E54B4B;
+  }
+`;
